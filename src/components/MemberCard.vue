dd61edc3c38874422826d290a86b7b8ee81c407d
evti <obit2004@hotmail.com>
2024-11-23 13:07:03 +0100

    member card

--- src/components/MemberCard.vue
+++ src/components/MemberCard.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="shadow-custom bg-zinc-900 rounded-3xl h-80 w-60 flex flex-col justify-center items-center">
+  <div class="bg-zinc-800  border-zinc-400 border-2 rounded-2xl h-80 w-60 flex flex-col justify-center items-center">
         <img class="w-3/5 rounded-full " src="../../public/petitcroco.png" alt="">
         <h1 class="text-gray-300">Thibaud Evrard</h1>
         <p class="text-gray-300">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim minima in, aperiam recusandae molestias quibusdam maiores eius? Nesciunt, quam placeat?</p>
