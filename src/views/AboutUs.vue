ac37a8b7b24e256e933cd5cf7c9aa5a89c65cbe4
evti <obit2004@hotmail.com>
2024-11-23 12:48:41 +0100

    About us vision and mission

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -3,7 +3,25 @@
 </script>
 
 <template>
-  <h1>About Us</h1>
+  <section class=" w-1/2 pt-28 pb-12 flex flex-col gap-5">
+        <h1 class="w-full text-center text-5xl text-green-300">
+            About Us
+        </h1>
+    </section>
+
+    <section class=" w-1/2 pt-12 pb-12 flex flex-col gap-5">
+        <h2 class="text-green-300">Our Mission</h2>
+        <p class="text-gray-200 text-justify">
+          Our mission at <bold class="font-bold text-green-300">EventPlanner+</bold> is to revolutionize the way events are organized and managed by providing a user-friendly and innovative platform. We aim to empower individuals and teams to seamlessly plan, coordinate, and execute events of any size, ensuring that every detail is handled efficiently and collaboratively.
+        </p>
+    </section>
+    
+    <section class=" w-1/2 pt-12 pb-12 flex flex-col gap-5">
+        <h2 class="text-green-300">Our Vision</h2>
+        <p class="text-gray-200 text-justify">
+          We envision <bold class="font-bold text-green-300">EventPlanner+</bold> as the go-to solution for event planning worldwide, fostering stronger connections and collaboration. By integrating advanced tools like real-time communication, interactive calendars, and dynamic management features, we aspire to make event planning not just a task, but an enjoyable and creative experience for everyone involved.
+        </p>
+    </section>
 
 </template>
 
