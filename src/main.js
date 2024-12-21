85ad416c8fbbb2a77f656f4be4d70c92dd29a82c
evti <obit2004@hotmail.com>
2024-12-21 14:16:15 +0100

    alert.css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import './style/button.css'
 import './style/input.css'
+import './style/alert.css'
 import App from './App.vue'
 import router from './router';
 import '@fortawesome/fontawesome-free/css/all.css';
