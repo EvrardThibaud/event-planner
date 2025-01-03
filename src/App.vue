36c1a6e8fb88c622910f516849ce9d659a3b7765
evti <obit2004@hotmail.com>
2025-01-03 15:54:36 +0100

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <script setup>
   import { useRoute,useRouter } from 'vue-router';
   import NavBar from './components/NavBar.vue'
+  import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <div id="alertsBox" class="h-auto w-auto fixed bottom-2 right-2 flex flex-col-reverse z-30"></div>
-  <header class="fixed h-14 w-screen bg-zinc-900 border-b-green-200 border-b-2 flex justify-between items-center">
+  <header class="fixed h-14 w-screen bg-zinc-900 border-b-green-200 border-b-2 flex justify-between items-center opacity-90">
     <RouterLink class="font-bold text-green-300 ml-4 mr-4 min-w-fit" :to="{name: 'Home'}" >
       EventPlanner+
     </RouterLink>
     <NavBar/>
   </header>
-  <div class="h-14 bg-red-500 w-full"></div>
+  <div class="h-14 w-full"></div>
   <main class="bg-zinc-900 ">
     <RouterView></RouterView>
   </main>
+  <Footer></Footer>
 </template>
 
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer>
+        <p>Copyright © 2025 EventPlanner+. All rights reserved.</p>
+    </footer>
+</template>
+
+<style scoped>
+</style>
